d7cda0f24fec04da1ac1c6ec09d8ba661bb8f953
Walnut Street <terry@Walnut-Streets-Mac-mini.local>
2013-01-27 10:10:32 -0500

    Added script for combining multiple videos

--- combineVideos.sh
+++ combineVideos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ffmpeg -i $1 -i color:d=S+${3} -i $2 \
+ffmpeg -i $1 -f lavfi -i color=d=${3}:size=426x320 -i $2 \
 -filter_complex '[0:0] [1:0] [2:0] concat=n=3:v=1:a=0 [v]' \
 -map '[v]' new.mp4 
