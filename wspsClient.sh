39fccd96e34ab20ea38eabf5f874fe9d585e2725
toddjobe@gmail.com <tjobe@linuxMint.(none)>
2013-05-24 08:48:58 -0400

    Got the audio running at least.

--- wspsClient.sh
+++ wspsClient.sh
@@ -3,6 +3,7 @@
 # Author: Todd Jobe
 # Todo: It's better to simply encode the mp4 locally, then let the
 # server encode the mp3 server-side
+# Todo: Use ducksboard.com to develop a near realtime dashboard for monitoring
 # Note: There doesn't seem to be a way to cause video recording to start
 # if the stream is not present.  So, if there's no video stream
 # it just won't create a video file
@@ -19,6 +20,7 @@ itsoffset=0
 webUser=tjobe
 #webHost=peopleforjesus.org
 webHost=localhost
+ffmpegExe = ""
 
 # Parse command line options
 while getopts "y" opt; do
@@ -38,7 +40,7 @@ dt=`date +%Y-%m-%d`
 os=`uname`
 localIP=""
 case $os in
-  Linux) localIP=`ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $1}'`;;
+  Linux) localIP=`ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | grep '192' | cut -d: -f2 | awk '{print $1}'`;;
   Darwin|FreeBSD|OpenBSD) localIP=`ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}'` ;;
 esac
 
@@ -99,7 +101,7 @@ function cleanup() {
   trap "" EXIT INT
   # back to front
   [[ ! -z "$ffmpeg_video_pid" ]] && kill -9 "${ffmpeg_video_pid}"
-  [[ ! -z "$ffmpeg_audio_pid" ]] && kill -9 "${ffmpeg_audio_pid}"
+  [[ ! -z "$lame_audio_pid" ]] && kill -9 "${lame_audio_pid}"
   [[ ! -z "$sox_pid" ]] && kill -9 "${sox_pid}"
   ssh ${streamUser}@${streamHost} "cmd /c taskkill /im \"ffmpeg.exe\" /f" 2>&1 >>${streamLog}
   [[ ! -z "$stream_pid" ]] && kill -9 "${stream_pid}"
@@ -108,23 +110,23 @@ function cleanup() {
 trap "cleanup" EXIT INT
 
 # plumbing though named pipes 
-audioFifo=/tmp/sox.wav
-ffmpegAudioFifo=/tmp/ffmpeg.mp3
+soxFifo=/tmp/sox.wav
+lameFifo=/tmp/ffmpeg.mp3
 ffmpegVideoFifo=/tmp/ffmpeg.mp4
 audioLocalFifo=/tmp/local.mp3
 videoLocalFifo=/tmp/local.mp4
 audioRemoteFifo=/tmp/remote.mp3
 videoRemoteFifo=/tmp/remote.mp4
 
-for fifo in "${audioFifo}" "${ffmpegAudioFifo}" "${ffmpegVideoFifo}" "${audioLocalFifo}" "${videoLocalFifo}" "${audioRemoteFifo}" "${videoRemoteFifo}" ; do
+for fifo in "${soxFifo}" "${lameFifo}" "${ffmpegVideoFifo}" "${audioLocalFifo}" "${videoLocalFifo}" "${audioRemoteFifo}" "${videoRemoteFifo}" ; do
   rm -f "$fifo"
   mkfifo "$fifo"
 done
 
 # log errors to separate places
 streamLog=/tmp/stream.log
-audioLog=/tmp/audio.log
-ffmpegAudioLog=/tmp/ffmpegAudio.log
+soxLog=/tmp/sox.log
+lameLog=/tmp/lame.log
 ffmpegVideoLog=/tmp/ffmpegVideo.log
 audioLocalLog=/tmp/audioLocal.log
 videoLocalLog=/tmp/videoLocal.log
@@ -136,33 +138,35 @@ ssh ${streamUser}@${streamHost} "cmd /c \"C:\\Program Files\\ffmpeg\\bin\\ffmpeg
 stream_pid=$!
 
 # audio recording command
-#sox -q -c 1 -t "${audioDriver}" "${audioDevice}" -t wav "${audioFifo}" remix - highpass 100 compand 0.05,0.2 6:-54,-90,-36,-36,-24,-24,0,-12 0 -90 0.1 &
-sox -t "${audioDriver}" "${audioDevice}" -t wav "${audioFifo}" channels 1 remix - highpass 100 compand 0.05,0.2 6:-54,-90,-36,-36,-24,-24,0,-12 0 -90 0.1 2>${audioLog} &
+#sox -q -c 1 -t "${audioDriver}" "${audioDevice}" -t wav "${soxFifo}" remix - highpass 100 compand 0.05,0.2 6:-54,-90,-36,-36,-24,-24,0,-12 0 -90 0.1 &
+sox -t "${audioDriver}" "${audioDevice}" -t wav "${soxFifo}" compand 0.2,0.20 5:-60,-40,-10 -5 -90 0.1 2>${soxLog} &
 sox_pid=$!
 
 # audio conversion command 
-#ffmpeg -threads 0 -i "${audioFifo}" -acodec mp3 -b:a 64k -y "${ffmpegAudioFifo}" & 
-ffmpeg -i "${audioFifo}" -acodec mp3 -b:a 64k -y "${ffmpegAudioFifo}" 2>${ffmpegAudioLog} & 
-ffmpeg_audio_pid=$!
+#ffmpeg -threads 0 -i "${soxFifo}" -acodec mp3 -b:a 64k -y "${lameFifo}" & 
+#ffmpeg -i "${soxFifo}" -acodec mp3 -b:a 64k -y "${lameFifo}" 2>${ffmpegAudioLog} & 
+lame -m s -a -q 7 -V 6 "${soxFifo}" "${lameFifo}" 2>${lameLog} & 
+
+lame_audio_pid=$!
 
 # video recording command
-ffmpeg -f mpegts -i "udp://localhost:${streamPort}" -itsoffset ${itsoffset} -f mp3 -i "${ffmpegAudioFifo}" -y -f mp4 -frag_duration 3600 "${ffmpegVideoFifo}" 2>${ffmpegVideoLog} &
+ffmpeg -f mpegts -i "udp://localhost:${streamPort}" -itsoffset ${itsoffset} -f mp3 -i "${lameFifo}" -y -f mp4 -frag_duration 3600 "${ffmpegVideoFifo}" 2>${ffmpegVideoLog} &
 ffmpeg_video_pid=$!
 
 # audio piping command
-tee ${audioLocalFifo} ${audioRemoteFifo} <${ffmpegAudioFifo} &
+tee ${audioLocalFifo} >${audioRemoteFifo} <${lameFifo} &
 audio_tee_pid=$!
 
 # video piping command 
-tee ${videoLocalFifo} ${videoRemoteFifo} <${ffmpegVideoFifo} &
+tee ${videoLocalFifo} >${videoRemoteFifo} <${ffmpegVideoFifo} &
 video_tee_pid=$!
 
 # audio ssh command
-ssh ${webUser}@${webHost} bash -c 'cat > "${remoteAudioFile}"' <${audioRemoteFifo} &
+ssh ${webUser}@${webHost} "bash -c 'cat > \"${remoteAudioFile}\"'" <${audioRemoteFifo} &
 audio_ssh_pid=$!
 
 # video ssh command
-ssh ${webUser}@${webHost} bash -c 'cat > "${remoteVideoFile}"' <${videoRemoteFifo} &
+ssh ${webUser}@${webHost} "bash -c 'cat > \"${remoteVideoFile}\"'" <${videoRemoteFifo} &
 video_ssh_pid=$!
 
 # audio local command
@@ -174,11 +178,11 @@ cat <${videoLocalFifo} >${localVideoFile} &
 video_local_pid=$!
 
 # wait for kill signal to stop recording
-wait ${stream_pid} ${sox_pid} ${ffmpeg_audio_pid} ${ffmpeg_video_pid} ${audio_tee_pid} ${video_tee_pid}\
+wait ${stream_pid} ${sox_pid} ${lame_audio_pid} ${ffmpeg_video_pid} ${audio_tee_pid} ${video_tee_pid}\
 ${audio_ssh_pid} ${video_ssh_pid} ${audio_local_pid} ${video_local_pid}
 
 # remove the named pipes
-for fifo in "${audioFifo}" "${ffmpegAudioFifo}" "${ffmpegVideoFifo}" "${audioLocalFifo}" "${videoLocalFifo}" "${audioRemoteFifo}" "${videoRemoteFifo}" ; do
+for fifo in "${soxFifo}" "${lameFifo}" "${ffmpegVideoFifo}" "${audioLocalFifo}" "${videoLocalFifo}" "${audioRemoteFifo}" "${videoRemoteFifo}" ; do
   rm -f "$fifo"
 done
 
